b60ef42eb95783c577d88ef0509f278398f822fd
Olalekan <olawal@groove.ng>
2025-07-14 12:53:29 +0100

    Fixed saved cached hls url

--- lib/src/audio_cache_manager.dart
+++ lib/src/audio_cache_manager.dart
@@ -201,9 +201,8 @@ class AudioCacheManager {
           return proxyUrl; // Serve the main manifest through the proxy
         } else if (entry.isHls) { // Unencrypted HLS: play directly from file
           final String localManifestPath = entry.hlsManifestFilePath!;
-          AppLogger.info('Returning HLS local manifest path (unencrypted): $localManifestPath '
-              '\n http://${_proxyServer.host}:${_proxyServer.port}/audio/$trackId', name: 'AudioCacheManager');
-          return 'http://${_proxyServer.host}:${_proxyServer.port}/audio/$trackId';
+          AppLogger.info('Returning HLS local manifest path (unencrypted): $localManifestPath', name: 'AudioCacheManager');
+          return 'file://$localManifestPath';
         }
         else {
           // Existing MP3 logic (also uses proxy for encrypted MP3s)
