9912f611129a97b69ace5f00861f4341252c06ab
Olalekan <olawal@groove.ng>
2025-08-06 15:18:15 +0100

    updating getUrl to proxy url at all time

--- lib/src/audio_cache_manager.dart
+++ lib/src/audio_cache_manager.dart
@@ -204,7 +204,8 @@ class AudioCacheManager {
         } else if (entry.isHls) { // Unencrypted HLS: play directly from file
           final String localManifestPath = entry.hlsManifestFilePath!;
           AppLogger.info('Returning HLS local manifest path (unencrypted): $localManifestPath', name: 'AudioCacheManager');
-          return 'file://$localManifestPath';
+          return _proxyServer.getProxyUrl(trackId) ;
+          // 'file://$localManifestPath';
         }
         else {
           // Existing MP3 logic (also uses proxy for encrypted MP3s)
